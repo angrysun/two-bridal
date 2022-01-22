e5c9da3aa356cce42d371870d624837abdd0b3ec
Keiran Brown <keiranbrown1301@gmail.com>
2022-01-22 20:10:17 +0900

    added some associations and dependent destroy scenarios

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
+  has_many :dresses, dependent: :destroy
+  has_many :bookmarks, dependent: :destroy
+  has_many :bookings_as_owner, through: dresses, source: bookings
+  has_many :reviews, through: dresses
+  has_many :bookings
+  # Need to double check with Teacher if these BOOKINGS associations are OK.
+  # Also, our dependent destroy callings
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
