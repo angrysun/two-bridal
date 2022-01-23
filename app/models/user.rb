b44d8d04c1ea29923e66022abf8fbeecfec058ba
Camille Fujisaki <cae224@cornell.edu>
2022-01-23 12:49:37 +0900

    added available column to dresses + fixed bug in user by adding missing colon to table names

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   has_many :dresses, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
-  has_many :bookings_as_owner, through: dresses, source: bookings
-  has_many :reviews, through: dresses
+  has_many :bookings_as_owner, through: :dresses, source: :bookings
+  has_many :reviews, through: :dresses
   has_many :bookings
   # Need to double check with Teacher if these BOOKINGS associations are OK.
   # Also, our dependent destroy callings
--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_22_100855) do
-
+ActiveRecord::Schema.define(version: 2022_01_23_034621) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-
-  create_table "bookmarks", force: :cascade do |t|
-    t.bigint "dress_id", null: false
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["dress_id"], name: "index_bookmarks_on_dress_id"
-    t.index ["user_id"], name: "index_bookmarks_on_user_id"
-  end
 
   create_table "bookings", force: :cascade do |t|
     t.bigint "dress_id", null: false
@@ -38,6 +27,15 @@ ActiveRecord::Schema.define(version: 2022_01_22_100855) do
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
+  create_table "bookmarks", force: :cascade do |t|
+    t.bigint "dress_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["dress_id"], name: "index_bookmarks_on_dress_id"
+    t.index ["user_id"], name: "index_bookmarks_on_user_id"
+  end
+
   create_table "dresses", force: :cascade do |t|
     t.string "brand"
     t.string "color"
@@ -48,6 +46,7 @@ ActiveRecord::Schema.define(version: 2022_01_22_100855) do
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "available", default: true
     t.index ["user_id"], name: "index_dresses_on_user_id"
   end
 
@@ -74,11 +73,10 @@ ActiveRecord::Schema.define(version: 2022_01_22_100855) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
-  add_foreign_key "bookmarks", "dresses"
-  add_foreign_key "bookmarks", "users"
   add_foreign_key "bookings", "dresses"
   add_foreign_key "bookings", "users"
+  add_foreign_key "bookmarks", "dresses"
+  add_foreign_key "bookmarks", "users"
   add_foreign_key "dresses", "users"
   add_foreign_key "reviews", "dresses"
 end
