a5484bd7e45695a340edb275f7ed05a04a20b3e0
Gary Yau <gary.yau@hotmail.com>
2022-01-26 02:02:19 +0900

    changed the routing of booking index, update, show

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
   resources :dresses do
-    resources :bookings, only: %i[new create show update] do
+    resources :bookings, only: %i[new create] do
     end
     resources :reviews, only: [:new, :create]
   end
-    resources :bookings, only: [:destroy] do
+    resources :bookings, only: %i[destroy index show update] do
+      # changed routing as index should be seperate. Show & Update should be associated with the correct user not the dress.
   end
     resources :reviews, only: [:destroy]
 end
