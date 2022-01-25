77d96aed0740fdbadaf6af843c321b46b446feb2
Gary Yau <gary.yau@hotmail.com>
2022-01-26 01:28:22 +0900

    added commented out booking methods in controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,9 +1,18 @@
 class BookingsController < ApplicationController
   before_action :find_dress, only: %i[new create destroy]
 
+  # def index
+  #   # @bookings = policy_scope(Booking).order(created_at: :desc)
+  #   @bookings = Booking.all
+  # end
+
   def new
     @booking = Booking.new
   end
+
+  # def show
+  #   @booking = Booking.find(params[id])
+  # end
 
   def create
     @booking = Booking.new(booking_params)
@@ -27,11 +36,15 @@ class BookingsController < ApplicationController
     # Changed path back to "bookings path""
   end
 
+  # def update
+  #   @booking.update(booking_params)
+  #   redirect_to booking_path(@booking)
+  # end
+
   private
 
   def booking_params
     params.require(:booking).permit(:starting_date, :ending_date, :status)
-    # permit status?
   end
 
   def find_dress
