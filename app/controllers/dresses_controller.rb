84bff3da2cbb719be668406da15bf489c0a00a16
Camille Fujisaki <cae224@cornell.edu>
2022-01-25 21:03:04 +0900

    added pundit

--- app/controllers/dresses_controller.rb
+++ app/controllers/dresses_controller.rb
@@ -1,18 +1,21 @@
 class DressesController < ApplicationController
   def index
-    @dresses = Dress.all
+    @dresses = policy_scope(Dress).order(created_at: :desc)
   end
 
   def show
     @dress = Dress.find(params[:id])
+    authorize @dress
   end
 
   def new
     @dress = Dress.new
+    authorize @dress
   end
 
   def create
     @dress = Dress.new(dress_params)
+    authorize @dress
     @dress.user = current_user
     if @dress.save
       redirect_to dress_path(@dress)
@@ -23,6 +26,7 @@ class DressesController < ApplicationController
 
   def destroy
     @dress = Dress.find(params[:id])
+    authorize @dress
     @dress.destroy
     flash[:remove] = "Successfully deleted"
     #thanks for the great idea Gary
