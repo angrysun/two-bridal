73d280be33e257cfafd4cd1e85769ad7bf7e0d73
Steven Vasquez <s.vasquez85@gmail.com>
2022-01-25 15:12:46 +0900

    Updated the dresses controller with new, create, and destroy actions as well as made a partial form for the Dress view

--- app/controllers/dresses_controller.rb
+++ app/controllers/dresses_controller.rb
@@ -6,4 +6,33 @@ class DressesController < ApplicationController
   def show
     @dress = Dress.find(params[:id])
   end
+
+  def new
+    @dress = Dress.new
+  end
+
+  def create
+    @dress = Dress.new(dress_params)
+    @dress.user = current_user
+    if @dress.save
+      redirect_to dress_path(@dress)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dress = Dress.find(params[:id])
+    flash[:remove] = "\"#{@dress.brand} dress\" deleted"
+    #thanks for the great idea Gary
+    @dress.destroy
+    redirect_to dresses_path
+  end
+
+  private
+
+  def dress_params
+    params.require(:dress).permit(:brand, :color, :size, :style, :description,
+                                  :price_per_day, :user_id, photos: [])
+  end
 end
