0448324a102d1d9eb45e0229441e340c00182e2a
Camille Fujisaki <cae224@cornell.edu>
2022-01-25 22:11:58 +0900

    reviews-controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
   resources :dresses do
-    resources :bookings, only: %i[new create edit update]
+    resources :bookings, only: %i[new create edit update] do
+    end
+    resources :reviews, only: [:new, :create]
   end
-  resources :bookings, only: [:destroy]
+    resources :bookings, only: [:destroy] do
+  end
+    resources :reviews, only: [:destroy]
 end
