25f3f2ab97f610e226b13103085d8618b89ab1dd
Gary Yau <gary.yau@hotmail.com>
2022-01-26 13:23:51 +0900

    uncommeneted flatpickr for testing

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,23 +1,26 @@
 class BookingsController < ApplicationController
-  before_action :find_dress, only: %i[new show create destroy]
+  before_action :find_dress, only: %i[new create destroy]
 
-  # def index
-  #   # @bookings = policy_scope(Booking).order(created_at: :desc)
-  #   @bookings = Booking.all
-  # end
+  def index
+    @bookings = policy_scope(Booking).order(created_at: :desc)
+  end
 
   def new
     @booking = Booking.new
+    authorize @booking
   end
 
-  # def show
-  #   @booking = Booking.find(params[id])
-  # end
+  def show
+    @booking = Booking.find(params[:id])
+    @dress = @dress = Dress.find(@booking.dress_id)
+    authorize @booking
+  end
 
   def create
     @booking = Booking.new(booking_params)
     @booking.dress = @dress
     @booking.user = current_user
+    authorize @booking
     if @booking.save
       redirect_to booking_path(@booking)
       # Changed path from "bookings_path to booking_path."
@@ -29,6 +32,7 @@ class BookingsController < ApplicationController
 
   def destroy
     @booking = Booking.find(params[:id])
+    authorize @booking
     flash[:remove] = "\"#{@dress.brand} dress\" removed from bookings"
     # flash message to notify dress has been removed from bookings
     @booking.destroy
@@ -36,10 +40,12 @@ class BookingsController < ApplicationController
     # Changed path back to "bookings path""
   end
 
-  # def update
-  #   @booking.update(booking_params)
-  #   redirect_to booking_path(@booking)
-  # end
+  def update
+    @booking = Booking.find(params[:id])
+    @booking.update(booking_params)
+    authorize @booking
+    redirect_to booking_path(@booking)
+  end
 
   private
 
--- app/policies/booking_policy.rb
+++ app/policies/booking_policy.rb
@@ -4,4 +4,28 @@ class BookingPolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def create?
+    true
+    # unless user(current_user) == dress.user_id(owner_id) | So that the owner cannot book their own dress
+  end
+
+  def show?
+    dress = Dress.find(record.dress_id)
+    owner = dress.user_id
+    record.user == user || owner == user
+  end
+
+  def update?
+    dress = Dress.find(record.dress_id)
+    owner = dress.user_id
+    record.user == user || owner == user
+    # This logic lets only the owner of the dress and person who made the booking auhtorized?
+  end
+
+  def destroy?
+    record.user == user
+    # only admin can destroy and have the status of the booking go to completed or cancelled? Similar to Amazon,order history cant be deleted
+  end
+
 end
