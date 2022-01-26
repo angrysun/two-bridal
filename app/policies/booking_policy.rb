8e58e82b1dc7480d323823452cd4e2fd004bc2af
Gary Yau <gary.yau@hotmail.com>
2022-01-26 19:39:29 +0900

    policy changed as update function need sorting out

--- app/policies/booking_policy.rb
+++ app/policies/booking_policy.rb
@@ -11,17 +11,15 @@ class BookingPolicy < ApplicationPolicy
   end
 
   def show?
-    dress = Dress.find(record.dress_id)
-    owner = dress.user_id
     record.user == user || owner == user
   end
 
-  def update?
-    dress = Dress.find(record.dress_id)
-    owner = dress.user_id
-    record.user == user || owner == user
-    # This logic lets only the owner of the dress and person who made the booking auhtorized?
-  end
+  # def update?
+  #   dress = Dress.find(record.dress_id)
+  #   owner = dress.user_id
+  #   record.user == user || owner == user
+  #   # This logic should only let the owner of the dress and person who made the booking auhtorized?
+  # end
 
   def destroy?
     record.user == user
