1991ee61b5343bb9df53e98e3ce823dbc14ccd3e
Steven Vasquez <s.vasquez85@gmail.com>
2022-01-27 19:57:07 +0900

    JS fixes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -21,7 +21,7 @@ ActiveStorage.start();
 import "bootstrap";
 import { initFlatpickr } from "../plugins/flatpickr";
 
-initFlatpickr();
+// initFlatpickr();
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
@@ -29,4 +29,5 @@ initFlatpickr();
 document.addEventListener("turbolinks:load", () => {
   // Call your functions here, e.g:
   // initSelect2();
+  initFlatpickr();
 });
