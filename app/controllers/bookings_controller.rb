1b52efe89b025eb3902370e314d5d97ce4588670
Gary Yau <gary.yau@hotmail.com>
2022-01-27 20:05:39 +0900

    updated bookings controller, changed the flatpickr JS, changed the forms.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,6 @@
 class BookingsController < ApplicationController
-  before_action :set_booking, only: %i[update destroy]
+  before_action :find_dress, only: %i[new create destroy]
+  before_action :find_booking, only: %i[update destroy]
 
 
   def index
@@ -13,12 +14,12 @@ class BookingsController < ApplicationController
 
   def create
     @booking = Booking.new(booking_params)
-    @dress = Dress.find(params[:dress_id])
+    authorize @booking
     @booking.dress = @dress
     @booking.user = current_user
     authorize @booking
     if @booking.save
-      redirect_to booking_path(@booking)
+      redirect_to profile_path
       # Changed path from "bookings_path to booking_path."
     else
       render :new
@@ -29,17 +30,17 @@ class BookingsController < ApplicationController
     @booking.status = booking_params[:status]
     authorize @booking
     @booking.update
-    redirect_to booking_path(@booking)
+    redirect_to profile_path
   end
 
-  def destroy
-    authorize @booking
-    flash[:remove] = "\"#{@booking.dress.brand} dress\" removed from bookings"
-    # flash message to notify dress has been removed from bookings
-    @booking.destroy
-    redirect_to bookings_path
-    # Changed path back to "bookings path""
-  end
+  # def destroy
+  #   authorize @booking
+  #   flash[:remove] = "\"#{@booking.dress.brand} dress\" removed from bookings"
+  #   # flash message to notify dress has been removed from bookings
+  #   @booking.destroy
+  #   redirect_to bookings_path
+  #   # Changed path back to "bookings path""
+  # end
 
   private
 
@@ -47,7 +48,11 @@ class BookingsController < ApplicationController
     params.require(:booking).permit(:starting_date, :ending_date, :status)
   end
 
-  def set_booking
+  def find_dress
+    @dress = Dress.find(params[:dress_id])
+  end
+
+  def find_booking
     @booking = Booking.find(params[:id])
   end
 end
