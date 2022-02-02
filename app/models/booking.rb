a5681059fa1d037d0773cd266a0b6deb6476525e
Gary Yau <gary.yau@hotmail.com>
2022-02-02 16:59:35 +0900

    remove duplicate validates :dress on bookings model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,7 @@ class Booking < ApplicationRecord
   belongs_to :dress
   belongs_to :user
 
-  validates :dress, :dress, :starting_date, :ending_date, :address, :post_code, :total_price, presence: true
+  validates :dress, :starting_date, :ending_date, :address, :post_code, :total_price, presence: true
   validates :post_code, length: { is: 7 }
 
   enum status: { pending: 0, accepted: 1, rejected: 2 }
