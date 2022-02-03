c96036acf08596d9f35c1fc1b18ce520255ab68b
Gary Yau <gary.yau@hotmail.com>
2022-02-03 22:08:00 +0900

    remove swal button

--- app/javascript/plugins/init_sweetalert.js
+++ app/javascript/plugins/init_sweetalert.js
@@ -2,7 +2,6 @@ import swal from "sweetalert";
 
 const initSweetalert = (selector, options = {}, callback = () => {}) => {
   const swalButton = document.querySelectorAll(selector);
-  console.log(swalButton);
   swalButton.forEach((element) => {
     element.addEventListener("click", () => {
       swal(options).then(callback);
--- app/javascript/plugins/swiper.js
+++ app/javascript/plugins/swiper.js
@@ -1,16 +1,16 @@
-new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    effect: "fade",
-    centeredSlides: true,
-    autoplay: {
-      delay: 3000,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-});
+// new Swiper(".mySwiper", {
+//     spaceBetween: 30,
+//     effect: "fade",
+//     centeredSlides: true,
+//     autoplay: {
+//       delay: 3000,
+//     },
+//     navigation: {
+//       nextEl: ".swiper-button-next",
+//       prevEl: ".swiper-button-prev",
+//     },
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+// });
