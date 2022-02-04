9f77f18424743b6beff4f12830dd96ecc4990cf5
Gary Yau <gary.yau@hotmail.com>
2022-02-04 21:29:02 +0900

    added custom validation for starting date is not in the past

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -6,5 +6,15 @@ class Booking < ApplicationRecord
   validates :post_code, length: { is: 7 }
   validates :total_price, numericality: { greater_than: 0 }
 
+  validate :start_date_cannot_be_in_the_past
+  # special validate class for booking, thats why theres no 's'
+
+
   enum status: { pending: 0, accepted: 1, rejected: 2 }
+
+  def start_date_cannot_be_in_the_past
+    if starting_date.present? && starting_date < Date.today
+      errors.add(:starting_date, 'cannot be in the past')
+    end
+  end
 end
