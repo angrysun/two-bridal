d62f59663557d9c5e796fd01dd83066275fea3e1
Gary Yau <gary.yau@hotmail.com>
2022-02-05 01:33:50 +0900

    added an if statement for the swiper with an Id on the carousel

--- app/javascript/plugins/swiper.js
+++ app/javascript/plugins/swiper.js
@@ -1,23 +1,27 @@
 const initSwiper = () => {
-  new Swiper(".mySwiper", {
-    spaceBetween: 30,
-    effect: "fade",
-    // slidesPerView: "auto",
-    // If not using fade and having regular slide effect, set slidesPerView to "auto" to have it centered after navigating multiple pictures
-    centeredSlides: true,
-    loop: true,
-    autoplay: {
-      delay: 3000,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
+  const swiper_present = document.getElementById("swiper_p");
+
+  if (swiper_present) {
+    new Swiper(".mySwiper", {
+      spaceBetween: 30,
+      effect: "fade",
+      // slidesPerView: "auto",
+      // If not using fade and having regular slide effect, set slidesPerView to "auto" to have it centered after navigating multiple pictures
+      centeredSlides: true,
+      loop: true,
+      autoplay: {
+        delay: 3000,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+  }
 };
 
 export { initSwiper };
