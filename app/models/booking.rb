22a059050991120196ef9d09f02a503ff9a27e17
Gary Yau <gary.yau@hotmail.com>
2022-02-04 21:49:22 +0900

    updated custom validations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,9 +4,8 @@ class Booking < ApplicationRecord
 
   validates :dress, :starting_date, :ending_date, :address, :post_code, :total_price, presence: true
   validates :post_code, length: { is: 7 }
-  validates :total_price, numericality: { greater_than: 0 }
 
-  validate :start_date_cannot_be_in_the_past
+  validate :start_date_cannot_be_in_the_past, :start_date_cannot_be_before_end_date
   # special validate class for booking, thats why theres no 's'
 
 
@@ -17,4 +16,11 @@ class Booking < ApplicationRecord
       errors.add(:starting_date, 'cannot be in the past')
     end
   end
+  def start_date_cannot_be_before_end_date
+    if starting_date.present? && ending_date < starting_date
+      errors.add(:starting_date, 'cannot be after Ending date')
+    end
+  end
+
+
 end
