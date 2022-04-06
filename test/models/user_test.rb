a483e8f21899d95fca1b6935e20cc8b57e062c3c
Camille Fujisaki <cae224@cornell.edu>
2022-04-06 15:54:54 +0900

    added first user model test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,8 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "full_name returns the capitalized first name and last name" do
+    user = User.new(first_name: "John", last_name: "Cena", email: "jc@gmail.com",password: "1234567")
+    assert_equal "John Cena", "#{user.first_name} #{user.last_name}"
+  end
 end
