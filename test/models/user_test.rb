ea39184a516ef5515586b0ec0a85c8f7ee03cee9
Camille Fujisaki <cae224@cornell.edu>
2022-04-06 16:04:50 +0900

    updated test descriptions

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,8 +1,13 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  test "full_name returns the capitalized first name and last name" do
-    user = User.new(first_name: "John", last_name: "Cena", email: "jc@gmail.com",password: "1234567")
+  test "User can be saved with valid information" do
+    user = User.new(first_name: "John", last_name: "Cena", email: "jc@gmail.com", password: "1234567")
     assert_equal "John Cena", "#{user.first_name} #{user.last_name}"
+  end
+
+  test "User is not saved with incomplete data" do
+    user = User.new(first_name: "John", last_name: "Cena", password: "1234567")
+    assert_not user.save # doesn't save the user without an email
   end
 end
