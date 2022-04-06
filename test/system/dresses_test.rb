1b2e2efce0d7250a6a8a69ab7a1ed94e5e2afed6
Camille Fujisaki <cae224@cornell.edu>
2022-04-06 10:10:17 +0900

    added dresses system tests

---
+++ test/system/dresses_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class DressesTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit dresses_url
+  #
+  #   assert_selector "h1", text: "Dress"
+  # end
+end
