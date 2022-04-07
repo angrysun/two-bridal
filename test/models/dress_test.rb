156753924892b4790cb957c1c6ebadc2a35be5d4
Camille Fujisaki <cae224@cornell.edu>
2022-04-07 13:06:52 +0900

    added dress and review model tests

--- test/models/dress_test.rb
+++ test/models/dress_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class DressTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Dress can be saved with valid information" do
+    dress = Dress.new(brand: "Versace", color: "White", size: 4, style: "Column", description: "Very pretty", price_per_day: 4000, user: User.first)
+    assert dress.save
+  end
+
+  test "Dress with incomplete data shouldn't save" do
+    dress = Review.new
+    assert_not dress.save
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 class UserTest < ActiveSupport::TestCase
   test "User can be saved with valid information" do
     user = User.new(first_name: "John", last_name: "Cena", email: "jc@gmail.com", password: "1234567")
-    assert_equal "John Cena", "#{user.first_name} #{user.last_name}"
+    assert user.save
   end
 
   test "User is not saved with incomplete data" do
