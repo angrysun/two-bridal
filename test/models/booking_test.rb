825ff71d831de4f189dcd4a10a7c327680387db6
Camille Fujisaki <cae224@cornell.edu>
2022-04-07 13:31:34 +0900

    added booking model test

--- test/models/booking_test.rb
+++ test/models/booking_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class BookingTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "Booking can be saved with valid information" do
+    booking = Booking.new(dress: Dress.first, user: User.first, starting_date: Date.today, ending_date: Date.tomorrow + 2, address: "Tokyo", post_code: "1234567", total_price: 50000)
+    assert booking.save
+  end
+
+  test "Booking with incomplete data shouldn't save" do
+    booking = Booking.new
+    assert_not booking.save
+  end
 end
